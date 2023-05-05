9b0566054d8312ce37f2472208d8768491a05297
Jershindemetrius <132642496+Jershindemetrius@users.noreply.github.com>
2023-05-05 11:51:34 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def calculator():
+    # ask user for input
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    # perform calculation
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+    else:
+        print("Invalid operator")
+        return
+
+    # print result
+    print("Result: {}".format(result))
+
+# run calculator
+calculator()
